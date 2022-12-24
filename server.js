f21dd0dc06a153e16c7aa816ef1e1674a9a11745
Okoro John Darlington <okorojohndarlington@gmail.com>
2022-12-24 04:29:39 +0100

    commit

--- server.js
+++ server.js
@@ -13,14 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-	cors({
-		origin: "https://advanced-mern-auth.onrender.com",
-
-		methods: "GET,POST,PUT,DELETE",
-		credentials: true,
-	})
-);
+app.use(cors());
 
 app.use("/auth", authRoutes);
 app.use("/profile", profileRoutes);
